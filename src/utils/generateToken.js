410edbc6050c4cf749de432a408931cff13bc208
Nunnasak <bownan1112@gmail.com>
2026-08-13 17:12:01 +0700

    Implement JWT token generation and cookie setup in generateToken function

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,5 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (userID, res) => {
     const payload = { id: userID }
-    const token = jwt.sign(payload, process.env.JWT_SECRET)
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.cookie("jwt", token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        samesite: "strict",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    })
+    return token;
 }
+
+module.exports = { generateToken };
