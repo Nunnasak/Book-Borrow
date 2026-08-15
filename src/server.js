f739fe16a2fa0c4556f6f473d26ddc952cd330d4
Nunnasak <bownan1112@gmail.com>
2026-08-15 14:43:53 +0700

    Set up server configuration and middleware for JSON parsing and URL encoding

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
+const { connectDB, disconnectDB } = require('./config/db.js')
 const express = require("express");
 const authRoutes = require("./route/authRoute.js")
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
 })
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
-
-
+app.use("/auth", authRoutes)
 
 
 process.on("unhandledRejection", (err) => {
